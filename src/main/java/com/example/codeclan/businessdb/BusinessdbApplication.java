a8f27f3672f6b275329f6c76bee0d232f5f0078a
Ryan <ryanfrost2015@gmail.com>
2020-01-21 09:55:08 +0000

    setup

---
+++ src/main/java/com/example/codeclan/businessdb/BusinessdbApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.businessdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codeclan/businessdb/BusinessdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.businessdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
